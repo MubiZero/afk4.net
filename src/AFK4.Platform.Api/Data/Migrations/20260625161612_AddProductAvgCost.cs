d8bb5fd5ee0032824a053fccbdff4f01163091f4
mukhamedov.m <mukhamedov.m@eskhata.com>
2026-06-25 21:16:51 +0500

    feat(inventory): миграция AddProductAvgCost (pos_products.AvgCostMinorUnits)

---
+++ src/AFK4.Platform.Api/Data/Migrations/20260625161612_AddProductAvgCost.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AFK4.Platform.Api.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProductAvgCost : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "AvgCostMinorUnits",
+                table: "pos_products",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AvgCostMinorUnits",
+                table: "pos_products");
+        }
+    }
+}
